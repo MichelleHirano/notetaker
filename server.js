dfd98e1a2d026c6cf79c6b00db8c14bf0193f943
MHirano <michelle@hiranohomes.com>
2021-04-25 12:44:47 -0700

    rando tests
    starter code svae button is not working

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(express.json());
 app.use(express.static('public'));
 
 //app.get
-app.get('/api/notes', (req, res) =>{
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
 });
 
@@ -22,9 +26,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
